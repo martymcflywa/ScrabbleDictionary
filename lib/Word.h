7884adfbd177298471f5964e2ba8ef00e1d1a69e
martymcflywa <doc.brown.83@gmail.com>
2018-03-09 16:11:00 +0800

    Documented and refactored Word.

--- lib/Word.h
+++ lib/Word.h
@@ -4,27 +4,43 @@
 #include "WordType.h"
 
 using namespace std;
+using namespace lib;
 
 namespace lib
 {
     class Word
     {
-		// keep default ctr private, params required when instantiating Word
-		Word();
         string _word;
         Type _type;
         string _definition;
         IPrinter& _printer;
 
-        static Type resolveType(const string& type);
-		static string validate(const string& input);
     public:
+	    /**
+         * \brief Constructs a Word object with values extracted from a source dictionary. 
+         * \param word The word itself.
+         * \param type The type of word, ie. adj, n, etc. Must not include brackets "[]".
+         * \param definition The definition of the word.
+         * \param printer Inject implementation of IPrinter so that Word is not coupled to implementation of printing.
+         */
         Word(const string& word, const string& type, const string& definition, IPrinter& printer);
-
-        string getWord() const;
+		/**
+		 * \brief Don't want anyone using this.
+		 */
+		Word() = delete;
+		string getWord() const;
         Type getType() const;
         string getDefinition() const;
         void printDefinition();
+	    /**
+		 * \brief Implementation of operator ==, really useful for unit tests.
+		 * \param that The other Word to compare to.
+		 * \returns true if both Words contain the same values.
+		 */
 		bool operator == (const Word& that) const;
+
+    private:
+		static Type resolveType(const string& type);
+		static string validate(const string& input);
     };
 }
