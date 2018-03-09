c46f3593f49645dcc5d5a09c611648251249fc10
martymcflywa <doc.brown.83@gmail.com>
2018-03-09 15:45:03 +0800

    Documented and refactored Dictionary.

--- lib/Dictionary.cpp
+++ lib/Dictionary.cpp
@@ -2,7 +2,62 @@
 #include "Dictionary.h"
 #include <regex>
 
-bool Dictionary::isEndOfEntry(const string line) const
+/**
+ * \brief Constructs the Dictionary. Responsible for loading entries from a source dictionary. Using dependency injection to decouple
+ * implementation of loading and printing.
+ * \param loader Inject implementation of ILoader to decouple Dictionary from implementation detail of loading. Allows flexibility if
+ * requirements ever change and the source changes to database rather than file.
+ * \param printer Same goes for IPrinter. Currently we are printing to console but what if future requirements change and we want to print as Json.
+ */
+Dictionary::Dictionary(ILoader& loader, IPrinter& printer) : _loader(loader), _printer(printer)
+{
+}
+
+/**
+ * \brief Load dictionary entries from a source. Depends on ILoader to retrieve the contents from the source.
+ * The function then iterates over each line, using regex to extract words, types and definitions.
+ * The currentLine counter and isEndOfEntry() is used to group together entries which extend over multiple lines.
+ * \param location The location of the dictionary source. Can be a filepath, db connection string etc. It depends on the injected ILoader.
+ * \returns The dictionary as map<string, Word> where the key is the actual word itself, and the value is the Word object. 
+ */
+map<string, Word> Dictionary::loadDictionary(const string& location) const
+{
+	auto& content = _loader.load(location);
+	string line;
+	auto output = map<string, Word>();
+
+	auto currentLine = 0;
+	string word;
+	string type;
+	string definition;
+
+	while (getline(content, line))
+	{
+		// the last blank line of the entry, time to construct the word and add it to collection.
+		if (isEndOfEntry(line))
+		{
+			output.insert(pair<string, Word>(word, buildWord(word, type, definition)));
+			currentLine = 0;
+			continue;
+		}
+
+		// the first line of the entry, grab the word and type
+		if (currentLine == 0)
+		{
+			word = extractWord(line);
+			type = extractType(line);
+			currentLine++;
+			continue;
+		}
+
+		// the second line of the entry, grab the definition
+		definition = extractDefinition(line);
+		currentLine++;
+	}
+	return output;
+}
+
+bool Dictionary::isEndOfEntry(string& line) const
 {
 	const regex pattern(END_OF_ENTRY_PATTERN, regex_constants::ECMAScript);
 	smatch match;
@@ -11,25 +66,25 @@ bool Dictionary::isEndOfEntry(const string line) const
 	return !match.empty();
 }
 
-string Dictionary::extractWord(const string line) const
+string Dictionary::extractWord(string& line) const
 {
 	const auto groupIndex = 1;
 	return extract(HEADER_PATTERN, line, groupIndex);
 }
 
-string Dictionary::extractType(string line) const
+string Dictionary::extractType(string& line) const
 {
 	const auto groupIndex = 2;
 	return extract(HEADER_PATTERN, line, groupIndex);
 }
 
-string Dictionary::extractDefinition(const string line) const
+string Dictionary::extractDefinition(string& line) const
 {
 	const auto groupIndex = 0;
 	return extract(DEFINITION_PATTERN, line, groupIndex);
 }
 
-string Dictionary::extract(string pattern, const string line, const int groupIndex) const
+string Dictionary::extract(string pattern, string& line, int groupIndex) const
 {
 	const regex regex(pattern, regex_constants::ECMAScript);
 	smatch match;
@@ -41,7 +96,7 @@ string Dictionary::extract(string pattern, const string line, const int groupInd
 	return match[groupIndex];
 }
 
-Word Dictionary::buildWord(string word, string type, string definition) const
+Word Dictionary::buildWord(string& word, string& type, string& definition) const
 {
 	if (word.empty())
 		throw invalid_argument("Word is empty");
@@ -50,44 +105,5 @@ Word Dictionary::buildWord(string word, string type, string definition) const
 	if (definition.empty())
 		throw invalid_argument("Definition is empty");
 
-	return Word(word, type, definition, _printer);
-}
-
-
-Dictionary::Dictionary(ILoader& loader, IPrinter& printer): _loader(loader), _printer(printer)
-{
-}
-
-map<string, Word> Dictionary::loadDictionary(const string& filepath) const
-{
-    auto& content = _loader.load(filepath);
-    string line;
-    auto output = map<string, Word>();
-
-	auto currentLine = 0;
-	string word;
-	string type;
-	string definition;
-
-    while(getline(content, line))
-    {
-		if (isEndOfEntry(line))
-		{
-			output.insert(pair<string, Word>(word, buildWord(word, type, definition)));
-			currentLine = 0;
-			continue;
-		}
-
-		if (currentLine == 0)
-		{
-			word = extractWord(line);
-			type = extractType(line);
-			currentLine++;
-			continue;
-		}
-		
-		definition = extractDefinition(line);
-		currentLine++;
-    }
-    return output;
+	return { word, type, definition, _printer };
 }
--- lib/Dictionary.h
+++ lib/Dictionary.h
@@ -1,29 +1,46 @@
 ﻿#pragma once
 #include <map>
+#include <regex>
 #include "Word.h"
 #include "ILoader.h"
-#include <regex>
 
 using namespace std;
 using namespace lib;
 
 namespace lib {
+
+	/**
+     * \brief A collection of Word objects loaded from a source (ie. text file).
+     */
     class Dictionary
     {
-        ILoader& _loader;
+	    ILoader & _loader;
 		IPrinter& _printer;
-	    bool isEndOfEntry(string line) const;
-		string extractWord(string line) const;
-		string extractType(string line) const;
-		string extractDefinition(string line) const;
-		string extract(string pattern, string line, int groupIndex) const;
-		Word buildWord(string word, string type, string definition) const;
-
 		const string HEADER_PATTERN = "^(\\S+) \\[(\\w+)\\]";
 		const string DEFINITION_PATTERN = "^.*";
 		const string END_OF_ENTRY_PATTERN = "^\\s*$";
-    public:
-        Dictionary(ILoader& loader, IPrinter& printer);
-        map<string, Word> loadDictionary(const string& filepath) const;
+
+	public:
+	    /**
+		 * \brief Constructs the Dictionary. Requires injection of ILoader and IPrinter.
+		 * \param loader the implementation of ILoader, responsible for loading dictionary entries from a source.
+		 * \param printer the implementation of IPrinter, responsible for printing the output of a Word.
+		 */
+		Dictionary(ILoader& loader, IPrinter& printer);
+	    /**
+		 * \brief Load dictionary entries from a source. Not concerned with what the source is, it is abstracted away into ILoader.
+		 * \param location The location of the dictionary source. Can be a filepath, db connection string etc.
+		 * The requirement depends on ILoader implementation.
+		 * \returns The dictionary as map<string, Word> where the key is the actual word itself, and the value is the Word object.
+		 */
+		map<string, Word> loadDictionary(const string& location) const;
+
+	private:
+	    bool isEndOfEntry(string& line) const;
+		string extractWord(string& line) const;
+		string extractType(string& line) const;
+		string extractDefinition(string& line) const;
+		string extract(string pattern, string& line, int groupIndex) const;
+		Word buildWord(string& word, string& type, string& definition) const;
     };
 }
