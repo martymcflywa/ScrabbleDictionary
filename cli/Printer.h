a7221f4d5552884f16cc782179cd05b1f9a98f50
Martin Ponce <doc.brown.83@gmail.com>
2018-03-11 18:26:59 +0800

    Printer adds prefix to output.

---
+++ cli/Printer.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <string>
+
+using namespace std;
+
+namespace cli
+{
+    /*
+     * \brief What type of message this is.
+     */
+    enum Prefix
+    {
+        Input,
+        Output,
+        Info,
+        Error
+    };
+
+    /**
+     * \brief Prints stuff to console.
+     */
+    class Printer
+    {
+    public:
+        /**
+         * \brief Need this overload so we can handle exception messages from .what()
+         * \param response The message response type.
+         * \param message The message to print.
+         */
+        static void print(Prefix response, char const* message);
+        /**
+         * \brief Overload to print messages passed in as std::string.
+         * \param response The message response type.
+         * \param message The message to print.
+         */
+        static void print(Prefix response, string& message);
+    private:
+        /**
+         * \brief Resolves prefix to string which is prepended to the message.
+         * \param response The message response type.
+         * \return The formatted prefix.
+         */
+        static string resolvePrefix(Prefix response);
+    };
+}
