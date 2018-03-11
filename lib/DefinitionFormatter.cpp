fc478b0adb3384753eddb4e7069fdfc974b970e5
Martin Ponce <doc.brown.83@gmail.com>
2018-03-11 18:34:48 +0800

    Moved to lib namespace.

--- lib/DefinitionFormatter.cpp
+++ lib/DefinitionFormatter.cpp
@@ -3,6 +3,9 @@
 #include "WordType.h"
 #include "Word.h"
 
+/**
+* \brief Formats the definition when printing to console.
+*/
 string DefinitionFormatter::format(Word& word)
 {
     return WordType::getName(word.getType()) + ":\n" +
