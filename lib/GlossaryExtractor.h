b9e0d04cef8e068e9dc05b57c0654e5b20b4d31c
Martin Ponce <doc.brown.83@gmail.com>
2018-03-19 08:16:30 +0800

    Glossary extractor implemented, passes BDD test.

---
+++ lib/GlossaryExtractor.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <vector>
+#include "IExtract.h"
+
+namespace lib
+{
+    /**
+     * \brief Implementation of IExtract, specific to extracting words from a text file,
+     * ignoring any special characters, punctuation etc. and converts words to lowercase.
+     */
+    class GlossaryExtractor : public IExtract<std::vector<std::string>, std::istream&>
+    {
+        /**
+         * \brief The stuff to ignore.
+         */
+        const std::string SPECIAL_CHARS = " \t\r\n,.!?;:-_“”\"+=@#$%^&*()[]<>\\/|`~";
+    public:
+        /**
+         * \brief Extracts words from a text file, ignoring any special characters,
+         * converting words to lowercase.
+         * \param content The stream from the source text file.
+         * \return A collection of lowercase words extracted from the text file.
+         */
+        std::vector<std::string> extract(std::istream& content) override;
+    };
+}
