746708fa40ad1d75f614f4a57dd40f733cae6422
Martin Ponce <doc.brown.83@gmail.com>
2018-03-20 14:38:47 +0800

    GlossaryWriter refactor, preparing for CLI integration.

---
+++ lib/GlossaryWriter.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include "GlossaryWriter.h"
+#include <fstream>
+#include "FileIoException.h"
+
+using namespace std;
+using namespace lib;
+
+GlossaryWriter::GlossaryWriter(const string& filepath) : _filepath(filepath)
+{
+}
+
+void GlossaryWriter::write(const string& content)
+{
+    auto file = ofstream(_filepath);
+
+    if(!file.is_open())
+        throw FileIoException("writing", _filepath);
+
+    file << content;
+    file.close();
+}
+
+std::string GlossaryWriter::getFilepath()
+{
+    return _filepath;
+}
---
+++ lib/GlossaryWriter.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "IWrite.h"
+#include <string>
+
+namespace lib
+{
+    class GlossaryWriter : public IWrite<const std::string&>
+    {
+        std::string _filepath;
+    public:
+        GlossaryWriter(const std::string& filepath);
+        void write(const std::string& content) override;
+        std::string getFilepath() override;
+    };
+}
+
