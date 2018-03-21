76c63e36a1b09a39dda2118f9913a876437121b6
Martin Ponce <doc.brown.83@gmail.com>
2018-03-21 11:37:16 +0800

    App and main updated to generate glossary async.

--- app/Application.cpp
+++ app/Application.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "../cli/CliUserInterface.h"
 #include "../cli/Logger.h"
+#include <future>
 
 using namespace std;
 using namespace app;
@@ -14,34 +15,49 @@ using namespace lib;
 */
 Application::Application() :
     // build the dictionary
-    _dictionaryFilepath(string(".\\dictionary2018.txt")),
-    _dictionaryReader(TextFileReader(_dictionaryFilepath)),
+    _dictionaryReader(TextFileReader(string(".\\dictionary2018.txt"))),
     _dictionaryFormatter(DefinitionFormatter()),
     _dictionaryPrinter(DefinitionPrinter(_dictionaryFormatter)),
     _dictionaryTask(DictionaryTask()),
     _dictionaryExtractor(DictionaryExtractor(_dictionaryPrinter, _dictionaryTask)),
     _dictionary(Dictionary(_dictionaryExtractor, _dictionaryTask)),
     // build the glossary
-    _usageFilepath(string(".\\many_english_works.txt")),
-    _rareWordFilepath(string(".\\new_work.txt")),
+    _usageReader(TextFileReader(string(".\\many_english_works.txt"))),
+    _rareWordReader(TextFileReader(string(".\\new_work.txt"))),
     _glossaryFormatter(GlossaryFormatter()),
     _glossaryExtractor(GlossaryExtractor()),
     _glossaryWriter(string(".\\glossary.txt")),
-    _glossary(Glossary(_dictionary, _glossaryExtractor, _glossaryFormatter, _glossaryWriter)),
+    _glossary(Glossary(
+        _dictionary, 
+        _usageReader, 
+        _rareWordReader, 
+        _glossaryExtractor, 
+        _glossaryFormatter, 
+        _glossaryWriter)),
     // build the ui
-    _ui(CliUserInterface(_dictionary, _usageFilepath, _rareWordFilepath, _glossary))
+    _ui(CliUserInterface(_dictionary, _glossary))
 {
 }
 
 /**
-* \brief Load the dictionary.
-*/
-void Application::init()
+ * \brief Call this synchronously, we want this to be completely populated before doing anything else.
+ */
+void Application::loadDictionary()
 {
     Logger::log(Info, "Loading dictionary...");
     _dictionary.loadDictionary(_dictionaryReader);
     const auto message = "Loaded dictionary with " + to_string(_dictionary.size()) + " entries";
     Logger::log(Info, message);
+}
+
+/**
+ * \brief Call this asynchronously. Glossary can be generated in the background while user interacts with
+ * rest of menu. Stop user from selecting glossary item in menu until this task is complete.
+ */
+void Application::generateGlossaryAsync()
+{
+    Logger::log(Info, "Generating glossary asynchronously...");
+    _glossary.generateAsync();
 }
 
 
