c718ae0456ddb4521f038633286d6aac214eae19
Martin Ponce <doc.brown.83@gmail.com>
2018-05-23 19:57:42 +0800

    Handle hyphenated words on extract.

--- lib/GlossaryExtractor.cpp
+++ lib/GlossaryExtractor.cpp
@@ -1,5 +1,6 @@
 ﻿#include "stdafx.h"
 #include <algorithm>
+#include <regex>
 #include <string>
 #include "GlossaryExtractor.h"
 
@@ -20,25 +21,113 @@ vector<string> GlossaryExtractor::extract(istream& content)
     while(!content.eof())
     {
         getline(content, line);
-        unsigned start = 0;
-        unsigned end;
 
-        do
-        {
-            start = line.find_first_not_of(SPECIAL_CHARS, start);
+        // TODO: solution here ensures hyphenated words are also captured.
+        // BUT it doubles the processing time. Could do with optimizing.
+        // IDEA: could using regex exclusively improve efficiency?
 
-            if (start == string::npos)
-                break;
+        // Old vs. new solution:
+        // Processing time: old = ~25s, new = ~45s
+        // Result: old = 155 entries, new = 149 entries
+        // After diffing both glossary.txt files, appears new solution
+        // is more correct, although there is still ambiguity with usage
+        // of '-' in source text files, so it's still not 100%.
 
-            end = line.find_first_of(SPECIAL_CHARS, start);
+        // words in text are sometimes delimited by "--", as well as " "
+        const auto onlySpaces = replace(line, "--", " ");
+        auto tokens = split(onlySpaces, SPECIAL_CHARS);
 
-            auto word = line.substr(start, end - start);
-            transform(word.begin(), word.end(), word.begin(), tolower);
+        // append to words
+        words.insert(words.end(), tokens.begin(), tokens.end());
+    }
+    return words;
+}
 
-            words.push_back(word);
-            start = end + 1;
-        }
-        while (end != string::npos);
+/**
+* \brief If found in the line, the 'pattern' will be replaced by 'replacement'.
+* \param line The line to search for the pattern.
+* \param pattern A regex pattern as a string. Must follow ECMAScript regex syntax.
+* TODO: Throw exception if incorrect syntax
+* \param replacement What to replace the pattern with in the line.
+* \return The line with the pattern replaced by the replacement.
+*/
+string GlossaryExtractor::replace(const string& line, const string& pattern, const string& replacement) const
+{
+    const auto rgx = regex(pattern, regex_constants::ECMAScript);
+    string output;
+    regex_replace(back_inserter(output), line.begin(), line.end(), rgx, replacement);
+    return output;
+}
+
+/**
+* \brief Splits a string into a vector of strings based on a delimiter.
+* \param line The line to split.
+* \param delimiter The delimiter to split on.
+* \return A vector of strings, without the delimiter.
+*/
+vector<string> GlossaryExtractor::split(string line, const string& delimiter)
+{
+    auto tokens = vector<string>{};
+
+    unsigned start = 0;
+    unsigned end;
+
+    do
+    {
+        start = line.find_first_not_of(delimiter, start);
+
+        if (start == string::npos)
+            break;
+
+        end = line.find_first_of(delimiter, start);
+        auto token = line.substr(start, end - start);
+        transform(token.begin(), token.end(), token.begin(), ::tolower);
+
+        if (!token.empty())
+            tokens.push_back(token);
+
+        start = end + 1;
+    }
+    while (end != string::npos);
+
+    return tokens;
+}
+
+/**
+* \brief Splits each string in the input vector to a flattened output vector,
+* based on a delimiter.
+* \param tokens The vector of tokens to split.
+* \param delimiter The delimiter to split on.
+* \return A flattened vector of strings, without the delimiter.
+*/
+vector<string> GlossaryExtractor::split(vector<string> tokens, const string& delimiter)
+{
+    auto words = vector<string>{};
+
+    for (const auto& token : tokens)
+    {
+        const auto splitTokens = split(token, delimiter);
+        words.insert(words.end(), splitTokens.begin(), splitTokens.end());
+    }
+    return words;
+}
+
+/**
+* \brief Erases the target from a collection of strings.
+* \param tokens The collection of strings to search and remove the target from.
+* \param target The target to delete.
+* \return The collection of strings with the target erased.
+*/
+std::vector<std::string> GlossaryExtractor::erase(std::vector<std::string> tokens, const char& target) const
+{
+    auto words = vector<string>{};
+
+    for (auto& token : tokens)
+    {
+        token.erase(remove(token.begin(), token.end(), target), token.end());
+
+        if (!token.empty())
+            words.push_back(token);
     }
     return words;
 }
--- lib/GlossaryExtractor.h
+++ lib/GlossaryExtractor.h
@@ -13,7 +13,7 @@ namespace lib
         /**
          * \brief The stuff to ignore.
          */
-        const std::string SPECIAL_CHARS = " \t\r\n,.!?;:-_“”\"+=@#$%^&*()[]{}<>\\/|`~ï»¿";
+        const std::string SPECIAL_CHARS = " \t\r\n,.!?;:_’“”\"+=@#$%^&*()[]{}<>\\/|`~ï»¿â€œÂ";
     public:
         /**
          * \brief Extracts words from a text file, ignoring any special characters,
@@ -22,5 +22,37 @@ namespace lib
          * \return A collection of lowercase words extracted from the text file.
          */
         std::vector<std::string> extract(std::istream& content) override;
+    private:
+        /**
+         * \brief If found in the line, the 'pattern' will be replaced by 'replacement'.
+         * \param line The line to search for the pattern.
+         * \param pattern A regex pattern as a string. Must follow ECMAScript regex syntax.
+         * TODO: Throw exception if incorrect syntax
+         * \param replacement What to replace the pattern with in the line.
+         * \return The line with the pattern replaced by the replacement.
+         */
+        std::string replace(const std::string& line, const std::string& pattern, const std::string& replacement) const;
+        /**
+         * \brief Splits a string into a vector of strings based on a delimiter.
+         * \param line The line to split.
+         * \param delimiter The delimiter to split on.
+         * \return A vector of strings, without the delimiter.
+         */
+        static std::vector<std::string> split(std::string line, const std::string& delimiter);
+        /**
+         * \brief Splits each string in the input vector to a flattened output vector,
+         * based on a delimiter.
+         * \param tokens The vector of tokens to split.
+         * \param delimiter The delimiter to split on.
+         * \return A flattened vector of strings, without the delimiter.
+         */
+        static std::vector<std::string> split(std::vector<std::string> tokens, const std::string& delimiter);
+        /**
+         * \brief Erases the target from a collection of strings.
+         * \param tokens The collection of strings to search and remove the target from.
+         * \param target The target to delete.
+         * \return The collection of strings with the target erased.
+         */
+        std::vector<std::string> erase(std::vector<std::string> tokens, const char& target) const;
     };
 }
